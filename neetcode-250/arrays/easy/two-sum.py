0b2adeeba3865f7f63ce6422a28393da1fb45ec3
Tomás Trincado <trincabros@gmail.com>
2025-12-29 09:35:31 -0300

    addition: two sum

---
+++ neetcode-250/arrays/easy/two-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        # num1 + num2 = target
+        # num2 = target - num1
+        # To extract num2 we need to calculate it for each index and save all num1 seen.
+
+        # Strategy: One-pass hash table that stores {value: index} of numbers we have seen so far.
+
+        seen = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+
+            if diff in seen:
+                return [seen[diff], i]
+
+            seen[num] = i
+
+        # Time: O(n) - We traverse the list containing n elements once.
+        # Space: O(n) - The extra space required depends on the number of items stored in the hash table.
