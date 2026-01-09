fd99c10a6a420fde8326caea57637e66289b0ef3
Tomás Trincado <trincabros@gmail.com>
2026-01-09 18:13:57 -0300

    revision: added comments

--- neetcode_250/arrays_hashing/easy/valid_anagram.py
+++ neetcode_250/arrays_hashing/easy/valid_anagram.py
@@ -2,6 +2,12 @@ from collections import defaultdict
 
 
 class Solution:
+    """
+    Uses a shared hashmap that counts the frequency of the letters in the word.
+
+    Time complexity: O(n) - We iterate through both strings once.
+    Space complexity:: O(1) - The map size stays within 26 characters (a-z).
+    """
     def isAnagram(self, s: str, t: str) -> bool:
         # If lengths differ, they cannot be anagrams.
         if len(s) != len(t):
@@ -18,7 +24,17 @@ class Solution:
             # Early exit: If count drops below zero, 't' has extra chars.
             if charmap[letter] < 0:
                 return False
-
-        # Time: O(n) - We iterate through both strings once.
-        # Space: O(1) - The map size stays within 26 characters (a-z).
         return True
+
+
+class Solution:
+    """
+    We can also sort them and compare for an O(nlogn+mlogm) time complexity, and the space complexity depends on the sorting
+    algorithm used. sorted() uses an hybrid derived from merge sort and insertion sort, which has a time complexity of O(nlogn)
+    in average and worst case, a O(n) best case and a worst case space complexity of O(n).
+    """
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        return sorted(s) == sorted(t)
--- neetcode_250/two_pointers/easy/reverse_string.py
+++ neetcode_250/two_pointers/easy/reverse_string.py
@@ -1,10 +1,26 @@
 class Solution:
     def reverseString(self, s: list[str]) -> None:
         """
-        Pretty straight forward two-pointer approach.
+        Pretty straight forward two-pointer approach with an O(n) time complexity and O(1) space complexity.
         """
         i, j = 0, len(s) - 1
         while i < j:
             s[i], s[j] = s[j], s[i]
             i += 1
             j -= 1
+
+class Solution2:
+    def reverseString(self, s: str) -> None:
+        """
+        There is a built-in function in python that reverses the list in O(n) & O(1), probably does
+        the two-pointer approach in the back.
+        """
+        return s.reverse()
+
+class Solution3:
+    def reverseString(self, s: str) -> None:
+        """
+        There is a version that gives as an input a string and not a list.
+        Here we can just do s[::-1], which is also O(n) and O(1).
+        """
+        return s[::-1]
