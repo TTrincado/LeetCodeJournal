bc2ae96386b5051be7cc16f8cd61ef1f1adc1ede
Tomás Trincado <trincabros@gmail.com>
2026-01-15 18:11:20 -0300

    revision: top k most frequent elements

--- neetcode_250/arrays_hashing/medium/top_k_frequent_elements.py
+++ neetcode_250/arrays_hashing/medium/top_k_frequent_elements.py
@@ -1,13 +1,47 @@
-from collections import Counter
-
 class Solution:
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
-        counter = Counter(nums) # O(n)
-        top_k_items = counter.most_common(k) # O(nlog(k))
-        return [item[0] for item in top_k_items]
-        
-        # Time complexity: O(nlog(k))
-        # Space complexity: O(n)
+        """
+        Uses Counter from collections for the most_common method, which sorts and counts the array.
 
-        # Another solution could be done with a frequency table, which would be
-        # O(n) in time and O(n) in space.
+        Time complexity: O(nlog(k))
+        Space complexity: O(n)
+        """
+        from collections import Counter
+
+        counter = Counter(nums)
+        top_k_items = counter.most_common(k)
+        return [item[0] for item in top_k_items]
+
+
+class Solution2:
+    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+        """
+        We use a counter and a frequency table to store the numbers.
+        In this frequency table, the index represents the frequency and the value the numbers that appear that many times.
+
+        The maximum number of times a number can be repeated is equal to the length of the list, so we can set the length of the
+        table to the length + 1 (to count for freq=0).
+
+        Time complexity: O(n)
+        Space complexity: O(n)
+        """
+        from collections import defaultdict
+
+        hashmap = defaultdict(int)
+        freq_table = [[] for i in range(len(nums) + 1)]
+
+        for num in nums:
+            hashmap[num] += 1
+
+        items = list(hashmap.items())
+
+        for i in range(len(items)):
+            freq_table[items[i][1]].append(items[i][0])
+
+        res = []
+        for i in range(len(freq_table) - 1, 0, -1):
+            for n in freq_table[i]:
+                res.append(n)
+
+                if len(res) == k:
+                    return res
