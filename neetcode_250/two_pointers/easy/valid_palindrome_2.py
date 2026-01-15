10ec35fd6953dbe76e832983af17b19d52df9621
Tomás Trincado <trincabros@gmail.com>
2026-01-15 08:44:54 -0300

    addition: optimal two pointer space complexity approach for valid palindrome 2

--- neetcode_250/two_pointers/easy/valid_palindrome_2.py
+++ neetcode_250/two_pointers/easy/valid_palindrome_2.py
@@ -2,6 +2,9 @@ class Solution:
     """
     Uses the same logic used in valid palindrome 1, but since we have 
     to check inside an if the time complexity rises to O(n^2).
+
+    Time complexity: O(n^2).
+    Space complexity: O(n), for poss.
     """
 
     def isPalindrome(self, s):
@@ -21,7 +24,10 @@ class Solution:
 
 class Solution2:
     """
-    Strategy: Check for a missmatch and check if forgiving that letter produces a palindrome.
+    Two-Pointer approach: Check for a missmatch and check if forgiving that letter produces a palindrome.
+
+    Time complexity: O(n).
+    Space complexity: O(n) for the storage of both skip strings.
     """
 
     def validPalindrome(self, s: str) -> bool:
@@ -36,5 +42,35 @@ class Solution2:
 
             i += 1
             j -= 1
+
+        return True
+
+
+class Solution3:
+    """
+    Same two-Pointer approach: Check for a missmatch and check if forgiving that letter produces a palindrome.
+
+    By creating a function that checks if it's a palindrome, we can manage to improve the space complexity to O(1).
+
+    Time complexity: O(n).
+    Space complexity: O(1).
+    """
+
+    def validPalindrome(self, s: str) -> bool:
+        def isPalindrome(l, r) -> bool:
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            if s[l] != s[r]:
+                return (isPalindrome(s, l + 1, r) or isPalindrome(s, l, r - 1))
+            l += 1
+            r -= 1
 
         return True
